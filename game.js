885555eba0d4684716fd7a181ff77a0715a5dcce
Bobby Spencer <bbdgtl@gmail.com>
2015-11-07 17:05:23 -0800

    commit changes

--- game.js
+++ game.js
@@ -7,66 +7,66 @@
 //Creating if/else statements for each ?
 
 //Adding functions to the game 11/06/2015
+//Moved ALL alerts into the DOM.
+var userName = prompt("What's your name?");
 
-function guessingGame() {
+function wannaPlay() {
+  var gameOn = prompt("Hello there, " + userName + ". Want to play a game?").toLowerCase();
+    if ((gameOn === "yes") || (gameOn === "y")) {
+      alert("Right on! Let's begin.");
+    } else {
+      alert("That's a shame, maybe next time!");
+      return;
+    }
 
-var userName = prompt("First of all, what's YOUR name?");
-var gameOn = prompt("Hello there, " + userName + ". Want to play a game?").toLowerCase();
-  if ((gameOn === "yes") || (gameOn === "y")) {
-    alert("Right on! Let's begin.");
-  } else {
-    alert("That's a shame, maybe next time!");
-    return;
-  }
+  var born = prompt(userName + ", was I born here in Washington?").toLowerCase();
+  console.log("correct answer is no");
+  alert("You guessed: " + born);
+    if ((born === "no") || (born === "n")) {
+      document.getElementById("alert1").innerHTML = "That's correct " + userName + ", I was born in the Philippines.";
+    } else {
+      document.getElementById("alert1").innerHTML = "I'm sorry, " + userName + ", but you're wrong. I was born in the Philippines.";
+    }
 
-var born = prompt(userName + ", was I born here in Washington?").toLowerCase();
-console.log("correct answer is no");
-alert("You guessed: " + born);
-  if ((born === "no") || (born === "n")) {
-    alert("That's correct " + userName + ", I was born in the Philippines.");
-  } else {
-    alert("I'm sorry, " + userName + ", but you're wrong. I was born in the Philippines.");
-  }
+  var age = prompt("Am I older than 30 years old?").toLowerCase();
+  console.log("correct answer is yes");
+  alert("You guessed: " + age);
+    if ((age === "yes") || (age === "y")) {
+      document.getElementById("alert2").innerHTML = "That's correct, " + userName + "! I am 35 (ugh) years old.";
+    } else {
+      document.getElementById("alert2").innerHTML = "That's not right " + userName + ", I am actually 35 years old.";
+    }
 
-var age = prompt("Am I older than 30 years old?").toLowerCase();
-console.log("correct answer is yes");
-alert("You guessed: " + age);
-  if ((age === "yes") || (age === "y")) {
-    alert("That's correct, " + userName + "! I am 35 (ugh) years old.");
-  } else {
-    alert("That's not right " + userName + ", I am actually 35 years old.");
-  }
-
-var pets = prompt("Do I have any pets?").toLowerCase();
-console.log("correct answer is yes");
-alert("You guessed: " + pets);
-  if ((pets === "yes") || (pets === "y")) {
-    alert("That's correct, " + userName + "! I have a big goofy dog named Rocky!");
-  } else {
-    alert("Wrong! " + userName + ", I have a dog named Rocky... do you even know me?");
-  }
+  var pets = prompt("Do I have any pets?").toLowerCase();
+  console.log("correct answer is yes");
+  alert("You guessed: " + pets);
+    if ((pets === "yes") || (pets === "y")) {
+      document.getElementById("alert3").innerHTML = "That's correct, " + userName + "! I have a big goofy dog named Rocky!";
+    } else {
+      document.getElementById("alert3").innerHTML = "Wrong! " + userName + ", I have a dog named Rocky... do you even know me?";
+    }
 
 //including a 4th ? to add a numerical value
-var timeHere = prompt("How many years have I lived in Seattle?");
-  console.log("13 years");
-  alert("You guessed: " + timeHere);
-    if (timeHere > "13") {
-      alert("Too high. How old do you think I am, " + userName + "?");
-    } else if (timeHere < "13") {
-      alert("Too low, sorry.");
-    } else {
-      alert("Correct! I've lived here for 13 years now.");
-    }
+  var timeHere = prompt("How many years have I lived in Seattle?");
+    console.log("13 years");
+    alert("You guessed: " + timeHere);
+      if (timeHere > "13") {
+        document.getElementById("alert4").innerHTML = "Too high. How old do you think I am, " + userName + "?";
+      } else if (timeHere < "13") {
+        alert("Too low, sorry.");
+      } else {
+        document.getElementById("alert4").innerHTML = "Correct! I've lived here for 13 years now.";
+      }
 
 //adding a 5th yes/no question to the game
-var commute = prompt("Do I live within 5 miles of Codefellows?");
-  console.log("No, I live 7 miles away.");
-  alert("You guessed: " + commute);
-    if ((commute === "yes") || (commute === "y")) {
-      alert("Nope. Sorry, " + userName + ". I live about 7 miles south of Codefellows.");
-    } else {
-      alert("Correct! I live about 7 miles south of Codefellows.");
-    }
+  var commute = prompt("Do I live within 5 miles of Codefellows?");
+    console.log("No, I live 7 miles away.");
+    alert("You guessed: " + commute);
+      if ((commute === "yes") || (commute === "y")) {
+        document.getElementById("alert5").innerHTML = "Nope. Sorry, " + userName + ". I live about 7 miles south of Codefellows.";
+      } else {
+        document.getElementById("alert5").innerHTML = "Correct! I live about 7 miles south of Codefellows.";
+      }
 
 //adding a 6th question to the game, this time with a numeric value.
 var ranNum = Math.floor(Math.random() * 10) + 1;
@@ -76,7 +76,7 @@ console.log("It is " + ranNum);
   while (turns > 0) {
     var guess = Number(prompt(tryAGuess + " You have " + turns + " chances."));
     if (guess === ranNum) {
-      alert("Correct! You win all the things!");
+      document.getElementById("alert6").innerHTML = "Correct! You win all the things!";
       break;
     } else {
       tryAGuess = "WRONG!";
@@ -90,7 +90,7 @@ console.log("It is " + ranNum);
   }
 }
 
-guessingGame();
+wannaPlay();
 
 
 
